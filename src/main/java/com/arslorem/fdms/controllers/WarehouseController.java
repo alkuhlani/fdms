3c92c8d628ee815224bc2f580c33350e3b3d7624
Ahmed Mohammed <ahmed.alkohlany@gmail.com>
2019-05-08 16:59:40 +0300

    Changes in the order of files
    make all stock web pages in one folder

--- src/main/java/com/arslorem/fdms/controllers/WarehouseController.java
+++ src/main/java/com/arslorem/fdms/controllers/WarehouseController.java
@@ -1,7 +1,11 @@
 package com.arslorem.fdms.controllers;
 
+import com.arslorem.fdms.entities.District;
 import com.arslorem.fdms.entities.Warehouse;
+import com.arslorem.fdms.services.DistrictService;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -13,4 +17,9 @@ import javax.inject.Named;
 public class WarehouseController extends SuperCRUDController<Warehouse> {
 
     private static final long serialVersionUID = 1L;
+    @Inject
+    private DistrictService districtService;
+    public List<District> getDistricts(){
+        return districtService.find();
+    }
 }
